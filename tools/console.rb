f0fcff0a914b1513b6b8793863ae07a87e9874e9
Lucas Montanari <Lmonty2011@gmail.com>
2020-11-20 11:01:44 -0800

    implemented VentureCapitalist class

--- tools/console.rb
+++ tools/console.rb
@@ -12,5 +12,10 @@ end
   # new_startup2 = Startup.new("marco", "att", "att.com")
   # new_startup3 = Startup.new("chris", "apple", "apple.com")
 
+  venture1 = VentureCapitalist.new("Lucas", 2000000000)
+  venture2 = VentureCapitalist.new("Riley", 3000000000)
+  venture3 = VentureCapitalist.new("Bob", 5)
+  venture4 = VentureCapitalist.new("Joe", 2000)
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
